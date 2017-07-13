e846bbae14bff9e89c9d5bb295e5ef92cc607e0d
John Moustakas <john.moustakas@gmail.com>
2017-07-12 21:17:39 -0400

    change debugging print statement

--- prospect/models/parameters.py
+++ prospect/models/parameters.py
@@ -123,8 +123,8 @@ class ProspectorParams(object):
             kwargs = self._config_dict[k].get('prior_args', {})
             this_prior = np.sum(func(theta[inds], **kwargs))
 
-            #if (not np.isfinite(this_prior)):
-            #    print('WARNING: ' + k + ' is out of bounds')
+            #if np.isinf(this_prior):
+            #    print('In models.ProspectorParams._prior_product parameter {} is out of bounds.'.format(k))
             lnp_prior += this_prior
         return lnp_prior
 
